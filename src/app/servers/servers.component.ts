ab82fa5d4f2016bb9566afdde8cfdb17daf705bc
Vastra <jessica_rosa@live.com.pt>
2023-08-16 22:31:10 +0200

    solution to the second exercise

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServerComponent } from '../server/server.component';
+import {FormsModule} from '@angular/forms'
 
 @Component({
   selector: '.app-servers',
@@ -7,8 +8,31 @@ import { ServerComponent } from '../server/server.component';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit{
-  constructor(){}
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
+  userName = '';
+  allowNewName = false;
+
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
   ngOnInit(): void {
     
+  }
+  onCreateServer(){
+    this.serverCreationStatus = 'Server '+this.serverName+' was created!';
+  }
+  onUpdateServerName(event: any){
+      this.serverName = event.target.value;
+  }
+  onUpdateUserName(event: any){
+    this.userName = event.target.value;
+    this.allowNewName = true;
+  }
+  onClearUserName(){
+    this.userName = '';
   }
 }
